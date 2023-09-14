53b59f7a58860f6b8544716cf3f90e156c0caa5a
Json-Ram <jasonram778@gmail.com>
2023-09-14 16:50:32 -0700

    Style TopMenu

--- app/layouts/includes/TopMenu.js
+++ app/layouts/includes/TopMenu.js
@@ -1,6 +1,7 @@
-'use client'
-import Link from "next/link"
-import { BsChevronDown } from 'react-icons/bs'
+'use client';
+import Link from "next/link";
+import { BsChevronDown } from 'react-icons/bs';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const TopMenu = () => {
   return (
@@ -19,9 +20,51 @@ const TopMenu = () => {
 
               <div
                 id='AuthDropDown'
-                className='absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg'
+                className='hidden absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg'
               >
+                <div className='flex items-center justify-start gap-1 p-3'>
+                  <img width={50} src='https://picsum.photos/id/1/200'/>
+                  <div className='font-bold text-[13px]'>
+                    Jason R
+                  </div>
+                </div>
+                <div className='border-b'></div>
+                <ul className='bg-white'>
+                  <li className='text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer'>
+                    <Link href='/orders'>
+                      My Orders
+                    </Link>
+                  </li>
+                  <li className='text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer'>
+                    Sign out
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className='px-3 hover:underline cursor-pointer'>
+              Daily Deals
+            </li>
+            <li className='px-3 hover:underline cursor-pointer'>
+              Need Help?
+            </li>
+          </ul>
 
+          <ul 
+            id='TopMenuRight'
+            className='flex items-center text-[11px] text-[#333333] px-2 h-8'
+          >
+            <li className='flex items-center gap-2 px-3 hover:underline cursor-pointer'>
+              <img width={32} src='/images/uk.png'/>
+              Ship to
+            </li>
+            <li className='px-3 hover:underline cursor-pointer'>
+              <div className='relative'>
+                <AiOutlineShoppingCart size={22} />
+                <div className='absolute text-[10px] -top-[2px] -right-[5px] bg-red-500 w-[14px] h-[14px] rounded-full text-white'>
+                  <div className='flex items-center justify-center -mt-[1px]'>
+                    5
+                  </div>
+                </div>
               </div>
             </li>
           </ul>
