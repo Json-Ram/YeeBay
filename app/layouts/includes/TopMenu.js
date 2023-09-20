299481ab2ad32a23b2f989ca9d069bb83f193a5d
Json-Ram <jasonram778@gmail.com>
2023-09-19 23:58:46 -0700

    Update TopMenu, Users can now Sign Out

--- app/layouts/includes/TopMenu.js
+++ app/layouts/includes/TopMenu.js
@@ -64,7 +64,9 @@ const TopMenu = () => {
                       My Orders
                     </Link>
                   </li>
-                  <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
+                  <li 
+                    onClick={() => { user.signOut(); setIsMenu(false) }}
+                    className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
                     Sign out
                   </li>
                 </ul>
