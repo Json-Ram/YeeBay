6d2488e5585d34331b197ab62cbc0b3a3281e27f
Json-Ram <jasonram778@gmail.com>
2023-09-26 00:13:48 -0700

    Update Main page, products from database now show

--- app/page.js
+++ app/page.js
@@ -2,25 +2,27 @@
 import MainLayout from "./layouts/MainLayout";
 import CarouselComp from "./components/CarouselComp";
 import Product from "./components/Product";
+import { useState, useEffect } from "react";
+import useIsLoading from "./hooks/useIsLoading";
 
 export default function Home() {
 
-  const products = [
-    {
-      id: 1,
-      title: "Test",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla massa lacus, eu eleifend eros consequat vel. Maecenas ornare fermentum lacus, sed pretium nisi hendrerit id. Etiam id leo sit amet ante dapibus tempor. Etiam id sapien sit amet sem mattis eleifend dignissim ut dolor. Pellentesque eu eros sed justo.",
-      url: "https://picsum.photos/id/7",
-      price: 2500
-    },
-    {
-      id: 2,
-      title: "Test2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla massa lacus, eu eleifend eros consequat vel. Maecenas ornare fermentum lacus, sed pretium nisi hendrerit id. Etiam id leo sit amet ante dapibus tempor. Etiam id sapien sit amet sem mattis eleifend dignissim ut dolor. Pellentesque eu eros sed justo.",
-      url: "https://picsum.photos/id/20",
-      price: 1900
-    }
-  ]
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    useIsLoading(true)
+
+    const response = await fetch("/api/products");
+    const prods = await response.json();
+
+    setProducts([]);
+    setProducts(prods);
+    useIsLoading(false);
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
 
   return (
     <MainLayout>
